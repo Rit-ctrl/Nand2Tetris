b0b4653f32124a92410bf91afa6508f6379b6a67
Rithic Kumar <53635318+Rit-ctrl@users.noreply.github.com>
2020-05-18 19:17:28 +0530

    Done with project 4 !! XD

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -7,3 +7,35 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+@0
+D=M
+@div
+M=D
+@prod
+M=0
+
+(LOOP)
+@1
+D=M
+@END
+D;JLE
+@div
+D=M
+@prod
+M=M+D
+@1
+M=M-1
+@LOOP
+0;JMP
+ 
+(END)
+@END
+@prod
+D=M
+@2
+M=D
+
+(TERMINATE)
+@TERMINATE
+0;JMP
+	
